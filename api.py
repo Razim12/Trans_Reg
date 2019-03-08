3293bb572dcf814c9df1e2161ec4db4441004d52
ABDUL QAYOOM RAZIM <35489549+Razim12@users.noreply.github.com>
2019-03-08 18:05:41 +0530

    Create api.py

---
+++ api.py
@@ -0,0 +1,25 @@
+import tornado.ioloop
+import tornado.web
+from transreg import transactionregister
+from addhandler import AddHandler
+from delhandler import DelHandler
+from gethandler import GetHandler
+
+transactions = transaction()
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("transaction Microservice v1")
+
+def make_app():
+    return tornado.web.Application([
+        (r"/v1", MainHandler),
+        (r"/v1/addtransreg", AddHandler, dict(transactions = transactions)),
+        (r"/v1/deltransreg", DelHandler, dict(transactions = transactions)),
+        (r"/v1/gettransactions", GetHandler, dict(transactions = transactions)),
+        ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
