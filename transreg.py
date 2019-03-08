f2466451dc79089d55230be17e7a70b57f43b11a
ABDUL QAYOOM RAZIM <35489549+Razim12@users.noreply.github.com>
2019-03-08 17:40:00 +0530

    Create transreg.py

---
+++ transreg.py
@@ -0,0 +1,31 @@
+import json
+
+
+class transactionregister:
+
+    def __init__(self):
+        self.transaction = []
+
+    def add_transaction(self, name, id):
+        new_transaction = {}
+        new_transaction["name"] = name
+        new_transaction["id"] = id
+        self.transactions.append(new_transaction)
+        print("transaction: {0}".format(new_transaction))
+        return json.dumps(new_transaction)
+
+    def del_transaction(self, name):
+        found = False
+        for idx, transsaction in enumerate(self.transactions):
+            if transaction["name"] == name:
+                index = idx
+                found = True
+                del self.transaction[idx]
+        print("transactions: {0}".format(json.dumps(self.transactions)))
+        return found
+
+    def get_all_transactions(self):
+        return self.transactions
+
+    def json_list(self):
+        return json.dumps(self.transactions)
