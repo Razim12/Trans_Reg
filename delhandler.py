70589b5a218ebdc8cf309326389dd762c3ab9c71
ABDUL QAYOOM RAZIM <35489549+Razim12@users.noreply.github.com>
2019-03-08 18:00:12 +0530

    Update delhandler.py

--- delhandler.py
+++ delhandler.py
@@ -11,8 +11,8 @@ class DelHandler(tornado.web.RequestHandler):
         name = self.get_argument('name')
         result = self.transactions.del_transaction(name)
         if result:
-            self.write("Deleted transaction name: {0} successfully".format(name))
+            self.write("Deleted User Name: {0} successfully".format(name))
             self.set_status(200)
         else:
-            self.write("transactionregister '{0}' not found".format(name))
+            self.write("Name '{0}' not found".format(name))
             self.set_status(404)
